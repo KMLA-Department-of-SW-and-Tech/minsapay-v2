28d9b5a240112d407b22f6cefc3cef24c6da47d1
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-07-15 11:38:48 +0900

    Readjust

--- src/components/moderator/user-info.jsx
+++ src/components/moderator/user-info.jsx
@@ -13,8 +13,8 @@ const Wrapper = styled.div`
   justify-content: flex-start;
   margin: 20px; /* 마진 추가 */
   position: fixed;
-  top: 15vh;
-  left: 50vw;
+  top: 13vh;
+  left: 45vw;
   z-index: 1000; /* 다른 요소 위에 표시되도록 z-index 설정 */
 `;
 
