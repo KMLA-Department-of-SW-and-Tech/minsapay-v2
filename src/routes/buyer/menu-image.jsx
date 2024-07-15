0183ce96b3c06e02ab9663e5f56a5e25b3b3a0b2
Yoo-Chan Jo <jo.yoochani@gmail.com>
2024-07-15 21:42:05 +0900

    Testing menu image background

--- src/routes/buyer/menu-image.jsx
+++ src/routes/buyer/menu-image.jsx
@@ -2,6 +2,15 @@ import { useNavigate } from "react-router-dom";
 import MenuImageRef from "../../images/MenuImage.png";
 import styled from "styled-components";
 
+const Wrapper = styled.div`
+  height: 100%;
+  width: 500px;
+  @media only screen and (max-width: 768px) {
+    width: 100vw;
+  }
+`;
+
+
 const CloseButton = styled.button`
   position: absolute;
   top: 1rem;
@@ -24,11 +33,11 @@ export default function MenuImage() {
   }
 
   return (
-    <>
+    <Wrapper style={{backgroundImage: `url(${MenuImageRef})`}}>
 
 
       <CloseButton onClick={onCloseButtonClick}>X</CloseButton>
-    </>
+    </Wrapper>
   );
   
 
